bd2be7c06b66d98900f7a164020040f7b9106f8b
payose <inipayose@gmail.com>
2025-02-01 20:44:13 +0100

    style adjustments for movie display card

--- src/components/core/movieCard.tsx
+++ src/components/core/movieCard.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+// import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { useState } from 'react';
 import { Stars, ThumbsUp } from 'lucide-react';
 import type { Movie } from '@/types/movie';
 import { Link } from 'react-router-dom'
@@ -6,47 +7,41 @@ import { Link } from 'react-router-dom'
 interface MovieCardProps {
     movie: Movie;
     onSelect?: (movie: Movie) => void;
-    className?: string;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ movie, className = '' }) => {
+const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     const moviePageProp = {
         movie: movie,
     };
 
+    const [imageError, setImageError] = useState(false);
+
     return (
         <Link to={`/movies/${movie.id}`} state = { moviePageProp }>
-            <Card
-                className={`bg-gray-800/50 border-gray-700 overflow-hidden group hover:border-blue-500 transition-all ${className}`}
-            >
-                <div className="relative aspect-[2/3] overflow-hidden h-[20rem]">
+            <div className="bg-transparent border-none">
+                <div className="relative aspect-[2/3] overflow-hidden h-[20rem] rounded-md">
                     <img
-                        src={movie.posterPath}
-                        alt={movie.title}
-                        className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+                        src={imageError ? `https://images.pexels.com/photos/29890776/pexels-photo-29890776/free-photo-of-traditional-vietnamese-new-year-gift-box.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2` 
+                                        : `http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        alt={`poster of ${movie.title}`}
+                        onError={() => setImageError(true)}
                     />
+
                     <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
                 </div>
-                <CardHeader>
-                    <CardTitle className="text-gray-100">{movie.title}</CardTitle>
-                    <CardDescription className="text-gray-400">
-                        {movie.release_date} | {movie.genres.join(', ')}
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <p className="text-gray-300 text-sm line-clamp-2">{movie.overview}</p>
-                </CardContent>
-                <CardFooter className="flex justify-between text-gray-400">
-                    <div className="flex items-center gap-2">
+                <div className='pt-2 pl-1'>
+                    <h3 className="text-gray-100">{movie.title}</h3>
+                    <p className="text-gray-400 text-xs pt-1">
+                        {new Date(movie.release_date).getFullYear()}
+                        {/* {movie.release_date} | {movie.genres.join(', ')} */}
+                    </p>
+                    <div className="flex items-center gap-1 pt-1">
                         <Stars className="h-4 w-4 text-yellow-500" />
-                        <span>{movie.vote_average}/10</span>
+                        <span className='text-gray-400 text-xs'>{movie.vote_average.toFixed(1)}/10</span>
                     </div>
-                    <div className="flex items-center gap-2">
-                        <ThumbsUp className="h-4 w-4" />
-                        <span>Recommended</span>
-                    </div>
-                </CardFooter>
-            </Card>
+                </div>
+                   
+            </div>
         </Link>
     );
 };
