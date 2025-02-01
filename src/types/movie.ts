dce61d44428c64496f326c95b348485819c7ba59
payose <inipayose@gmail.com>
2025-02-01 20:45:04 +0100

    key name adjustments

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,17 +1,13 @@
 export interface Movie {
-    id: string;
+    id: number | string; 
     title: string;
     release_date: string;
     overview: string;
-    vote_average: number;
-    genres: string[];
-    posterPath: string;
+    poster_path: string;
     backdrop_path?: string;
     genre_ids?: number[];
-    popularity?: number;
+    video: boolean,
+    vote_average: number;
     vote_count?: number;
-    adult?: boolean;
     original_language?: string;
-    original_title?: string;
 }
-  
